9f37a6ff34d809397de6bc679a9578f9cdb90230
kaushaljw <47314943+kaushaljw@users.noreply.github.com>
2019-02-21 12:40:46 +0530

    Update prediction.py

--- prediction.py
+++ prediction.py
@@ -8,13 +8,13 @@ Enter value2233445
 [2947353.58406281]
 '''
 
-tc = str(input("Enter value"))
-from numpy import array
-tc = [tc]
-t = array(tc)
-print(t.shape)
-t = t.reshape((t.shape[0],1))
-print(t.shape)
-t = t.astype(np.float64)
-total_revenue = regression_model.predict(t)
+tc = int(input("Enter value"))
+#from numpy import array
+#tc = [tc]
+#t = array(tc)
+#print(t.shape)
+#t = t.reshape((t.shape[0],1))
+#print(t.shape)
+#t = t.astype(np.float64)
+total_revenue = regression_model.predict([[tc]])
 print(total_revenue)
