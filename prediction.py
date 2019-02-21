07ff9594e9b5fba8db58725c7c4b3671c8c741b1
kaushaljw <47314943+kaushaljw@users.noreply.github.com>
2019-02-21 11:47:22 +0530

    Create prediction.py

---
+++ prediction.py
@@ -0,0 +1,20 @@
+
+''' Prediction for total revenue by using total cost '''
+
+'''Output:-
+Enter value2233445
+(1,)
+(1, 1)
+[2947353.58406281]
+'''
+
+tc = str(input("Enter value"))
+from numpy import array
+tc = [tc]
+t = array(tc)
+print(t.shape)
+t = t.reshape((t.shape[0],1))
+print(t.shape)
+t = t.astype(np.float64)
+total_revenue = regression_model.predict(t)
+print(total_revenue)
